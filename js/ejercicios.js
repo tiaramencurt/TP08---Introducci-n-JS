99ef14eebb8d6d9d4be1ccb826f8a1259abbf3b3
Maica <maica.trutner@gmail.com>
2025-09-09 20:05:40 -0300

    style.css, chat love you!!!

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -2,25 +2,25 @@ const input = document.getElementById("opciones");
 const resultado = document.getElementById("resultado");
 
 function ej1pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese su nombre: </p><input type='text' id='nombre'>";
     input.innerHTML += "<p>Ingrese su fecha de nacimiento: </p><input type='date' id='fecha'>";
     input.innerHTML += "<button onclick='ej1()'>Enviar</button>";
 }
 
 function ej2pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese una fruta:</p><input type='text' id='fruta'>";
     input.innerHTML += "<button onclick='ej2()'>Enviar</button>";
 }
 
 function ej3pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<button onclick='ej3()'>Ver comparaciones</button>";
 }
 
 function ej4pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese el nombre de la ciudad: </p><input type='text' id='nombre'>";
     input.innerHTML += "<p>Ingrese la fecha de fundación de la ciudad: </p><input type='date' id='fechaFundacion'>";
     input.innerHTML += "<p>Ingrese la población de la ciudad: </p><input type='number' id='poblacion'>";
@@ -29,25 +29,25 @@ function ej4pc() {
 }
 
 function ej5pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese una lista de números separados por coma: </p><input type='text' id='numeros'>";
     input.innerHTML += "<button onclick='ej5()'>Enviar y mostrar dobles</button>";
 }
 
 function ej6pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese la base de los triangulos a dibujar: </p><input type='number' id='baseTriangulos'>";
     input.innerHTML += "<button onclick='ej6()'>Dibujar triángulos</button>";
 }
 
 function ej7pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese una lista de nombres separados por coma: </p><input type='text' id='nombres'>";
     input.innerHTML += "<button onclick='ej7()'>Enviar y mostar nombres que empiezan por 'A'</button>";
 }
 
 function ej8pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese un texto: </p><input type='text' id='texto'><br>";
     input.innerHTML += "<p>Palabra a reemplazar: </p><input type='text' id='buscar'><br>";
     input.innerHTML += "<p>Palabra nueva: </p><input type='text' id='reemplazo'><br>";
@@ -55,20 +55,20 @@ function ej8pc() {
 }
 
 function ej9pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese un texto: </p><input type='text' id='texto'>";
     input.innerHTML += "<p>Ingrese un número: </p><input type='number' id='num'>";
     input.innerHTML += "<button onclick='ej9()'>Enviar</button>";
 }
 
 function ej10pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese una lista de elementos separados por coma: </p><input type='text' id='lista'>";
     input.innerHTML += "<button onclick='ej10()'>Enviar</button>";
 }
 
 function ej11pc() {
-    resultado.innerHTML = null;
+    resultado.innerHTML = "<h1>Resultado</h1>";
     input.innerHTML = "<p>Ingrese pedidos en formato 'nombre:total' separados por coma: </p><input type='text' id='pedidos'>";
     input.innerHTML += "<button onclick='ej11()'>Enviar y calcular total</button>";
 }
